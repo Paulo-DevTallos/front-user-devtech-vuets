2fb8311f30667d04f3690a48eadc4340edfec4f6
Paulo Sergio <pauloserg861@gmail.com>
2023-02-18 14:40:33 -0300

    aplicando estrutura de template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <RouterView />
+  <div class="container-layout">
+    <RouterView />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-})
+  name: "App",
+});
 </script>
+
+<style lang="scss">
+.container-layout {
+  max-width: 1350px;
+  margin: 0 auto;
+  background: red;
+}
+</style>
