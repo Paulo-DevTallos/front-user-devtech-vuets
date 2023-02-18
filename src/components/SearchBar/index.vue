2a6593951af84857b83aa67858f3c7dc2d378a43
Paulo Sergio <pauloserg861@gmail.com>
2023-02-18 16:33:37 -0300

    concluindo estilizacao da search bar

--- src/components/SearchBar/index.vue
+++ src/components/SearchBar/index.vue
@@ -22,10 +22,10 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .input-style {
   width: 90%;
-  height: 50px;
+  height: 45px;
   display: flex;
   border: 2px solid #009acc;
   border-radius: 25px;
@@ -44,14 +44,15 @@ export default defineComponent({
   }
 
   input {
-    width: 60%;
+    width: 75%;
   }
 
   .search-icon {
     background: #009acc;
+    border: 2px solid #009acc;
     width: 10%;
-    border-top-right-radius: 20px;
-    border-bottom-right-radius: 20px;
+    border-top-right-radius: 18px;
+    border-bottom-right-radius: 18px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -59,7 +60,7 @@ export default defineComponent({
   }
 
   svg {
-    font-size: 30px;
+    font-size: 27px;
     color: #fff;
   }
 }
