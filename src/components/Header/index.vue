51b2f33ff5df058a49800258947500a676082b05
Paulo Sergio <pauloserg861@gmail.com>
2023-02-18 16:34:00 -0300

    implementando carrinho de compras

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="header-actions">
       <div>
-        <span>(Nome do usuário) bem vindo(a) ao DevTech Marketplace</span>
+        <span>(Nome do usuário) bem vindo(a) ao E-tech Marketplace</span>
       </div>
       <nav class="nav-actions">
         <ul>
@@ -27,16 +27,44 @@
         </ul>
       </nav>
     </div>
+    <div class="header-application">
+      <div class="logo">
+        <img :src="logo" alt="logo E-tech" />
+      </div>
+      <div class="search-bar">
+        <Icon icon="mdi:menu" />
+        <SearchBar />
+      </div>
+      <div class="profile-actions">
+        <Icon icon="carbon:favorite" />
+        <div class="cart">
+          <Icon icon="mdi:cart-outline" />
+          <span class="cart-notification">0</span>
+          <!--essa notificacao só vai aparecer com o usuario logado-->
+          <span class="sum-cartvalue">R$ 0,00</span>
+          <!--esse valor só vai aparecer com o usuario logado-->
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Icon } from "@iconify/vue";
+import SearchBar from "@/components/SearchBar/index.vue";
+
+import logo from "@/assets/img/logo-dev-tech-nobg.png";
 
 export default defineComponent({
   name: "HeaderApp",
-  components: { Icon },
+  components: { Icon, SearchBar },
+  data() {
+    return {
+      logo: logo,
+    };
+  },
+
   methods: {
     register() {
       alert("chamará o form de cadastro");
@@ -48,7 +76,7 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .header {
   .header-actions {
     padding: 5px 0;
@@ -87,6 +115,70 @@ export default defineComponent({
             }
           }
         }
+      }
+    }
+  }
+
+  .header-application {
+    padding: 30px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+      img {
+        width: 200px;
+      }
+    }
+
+    .search-bar {
+      display: flex;
+      align-items: center;
+      width: 65%;
+
+      svg {
+        width: 10%;
+        font-size: 40px;
+        cursor: pointer;
+      }
+    }
+
+    .profile-actions {
+      font-size: 30px;
+      display: flex;
+      align-items: center;
+
+      > svg {
+        font-size: 30px;
+        margin-right: 30px;
+      }
+
+      .cart {
+        position: relative;
+        .cart-notification {
+          position: absolute;
+          bottom: 0;
+          left: 10%;
+          background: red;
+          color: #fff;
+          font-size: 0.8rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 20px;
+          height: 20px;
+          border-radius: 100%;
+        }
+
+        svg {
+          font-size: 27px;
+        }
+      }
+
+      .sum-cartvalue {
+        margin-left: 10px;
+        font-size: 1.3rem;
+        font-weight: 500;
       }
     }
   }
