e2f0793a08eee40e7545f5cc8578e8dd97a7c858
Paulo Sergio <pauloserg861@gmail.com>
2023-02-19 14:20:31 -0300

    implementando icones ao baseinput

--- src/components/Inputs/BaseInput.vue
+++ src/components/Inputs/BaseInput.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="input-container">
+    <div class="icon-area">
+      <Icon :icon="`carbon:${icon_name}`" />
+    </div>
     <input
       v-bind="$attrs"
       v-model="inputValue"
@@ -17,11 +20,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Icon } from "@iconify/vue";
 import RevellingPassword from "@/components/Icons/PasswordRevelling.vue";
 
 export default defineComponent({
   name: "BaseInput",
-  components: { RevellingPassword },
+  components: { RevellingPassword, Icon },
   emits: ["handleValue"],
   props: {
     currentValue: {
@@ -29,6 +33,9 @@ export default defineComponent({
     },
     hiddenRevellingPassword: {
       type: Boolean,
+    },
+    icon_name: {
+      type: String,
     },
   },
   data() {
@@ -55,3 +62,37 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.input-container {
+  height: 45px;
+  border: 1px solid #d1d1d1;
+  border-radius: 24px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+
+  .icon-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 12%;
+    height: 100%;
+    border-top-left-radius: 24px;
+    border-bottom-left-radius: 24px;
+    background: #d1d1d1;
+
+    > svg {
+      font-size: 1.3rem;
+    }
+  }
+
+  input {
+    outline: none;
+    border: none;
+    width: 80%;
+    padding-left: 12px;
+    font-size: 1rem;
+  }
+}
+</style>
