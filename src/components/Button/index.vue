860e9ee3f9caa14668c2e99aeb29144c3896b624
Paulo Sergio <pauloserg861@gmail.com>
2023-02-19 14:19:19 -0300

    criando componente de botao

---
+++ src/components/Button/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>Clique aqui</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainButton",
+});
+</script>
