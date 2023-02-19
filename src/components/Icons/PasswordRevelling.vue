756771fab047f3323230529f690027d9749a6be7
Paulo Sergio <pauloserg861@gmail.com>
2023-02-19 13:40:02 -0300

    criando revelador de senha

---
+++ src/components/Icons/PasswordRevelling.vue
@@ -0,0 +1,28 @@
+<template>
+  <span @click="(_$event) => $emit('toggle')">
+    <Icon icon="carbon:view" id="view-icon" v-if="isPassword" />
+    <Icon icon="carbon:view-off" id="view-icon" v-else />
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Icon } from "@iconify/vue";
+export default defineComponent({
+  name: "PasswordRevelling",
+  components: { Icon },
+  emits: ["toggle"],
+  props: {
+    isPassword: {
+      type: Boolean,
+    },
+  },
+});
+</script>
+
+<style>
+svg {
+  display: flex;
+  cursor: pointer;
+}
+</style>
